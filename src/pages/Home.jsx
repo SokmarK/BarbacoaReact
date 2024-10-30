302bff9077a31fa602c2226dd31c869d71542086
Juan Pablo Solano <hello@wondercraft.co>
2024-10-30 15:35:39 -0500

    meeting 001

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,34 @@
 import "./Home.scss";
-
+import Sample from "../components/Sample/Sample";
 export default function Home() {
-  return (
-    <section className="section">
-  <div className="section__div">
-    <p className="section__pneu">TU BUSQUEDA ACABÓ</p>
-    <p className="section__pmax">ASADORES DE </p>
-    <p className="section__pmax1">BARRIL</p>
-    <p className="pmin">Transforma tus parrilladas con un <br></br>ahumado perfecto. Nuestros ahumadores están diseñados para ofrecerte la máxima calidad y sabor en cada asado.</p>
-    <button>CONTACTANOS</button>
-  </div>
-  <div className="section__img-container">
-    <img src="barbacoamain.jpg" alt="" className="section__img" />
-  </div>
-</section>
+    return (
+        <>
+            <section className="section">
+                <div className="section__container container">
+                    <h1>
+                        <p className="section__pneu">TU BUSQUEDA ACABÓ</p>{" "}
+                        <br />
+                        <p className="section__pmax">
+                            ASADORES DE BARRIL ASD SAD SA DSAD
+                        </p>
+                    </h1>
+                    <p className="pmin">
+                        Transforma tus parrilladas con un <br></br>ahumado
+                        perfecto. Nuestros ahumadores están diseñados para
+                        ofrecerte la máxima calidad y sabor en cada asado.
+                    </p>
+                    <button>CONTACTANOS</button>
+                </div>
+                <div className="section__img-container">
+                    {/* <img
+                        src="barbacoamain.jpg"
+                        alt=""
+                        className="section__img"
+                    /> */}
+                </div>
+            </section>
 
-  );
+            <Sample />
+        </>
+    );
 }
